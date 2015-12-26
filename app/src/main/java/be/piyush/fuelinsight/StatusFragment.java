e619ee30dc5bd4b52d3986cdb2f3159e42023054
Piyush <pgkaila@gmail.com>
2015-12-26 12:10:07 +0530

    Added Chart in status activity

--- app/src/main/java/be/piyush/fuelinsight/StatusFragment.java
+++ app/src/main/java/be/piyush/fuelinsight/StatusFragment.java
@@ -1,15 +1,19 @@
 package be.piyush.fuelinsight;
 
-import android.database.Cursor;
+import android.graphics.Color;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import be.piyush.fuelinsight.data.refuel.FuelContract;
-import be.piyush.fuelinsight.data.refuel.FuelProvider;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+
+import java.util.ArrayList;
 
 /**
  * Created with Android Studio.
@@ -23,18 +27,78 @@ import be.piyush.fuelinsight.data.refuel.FuelProvider;
  */
 public class StatusFragment extends Fragment {
 
+    private LineChart mChart;
+
     public StatusFragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        String[] projection = { FuelContract.COLUMN_AMOUNT, FuelContract.COLUMN_ID };
-        Cursor fuelData = inflater.getContext().getContentResolver().query(FuelProvider.FUEL_CONTENT_URI, projection, null, null, null);
-        while (fuelData.getCount()!=0) {
-            fuelData.moveToNext();
-            Log.i("FuelData", "ID: " + fuelData.getString(1) + "  Amount: " + fuelData.getString(0));
-        }
-        return inflater.inflate(R.layout.fragment_status, container, false);
+
+
+
+        View view = inflater.inflate(R.layout.fragment_status, container, false);
+
+        //TODO Need lot of design changes in chart
+        //TODO Have to update LineChart with CombineChart to display LineChart with BarChart
+        mChart = (LineChart) view.findViewById(R.id.chartInsight);
+        mChart.setDescription("");
+        mChart.setBackgroundColor(Color.WHITE);
+        mChart.setDrawGridBackground(false);
+
+        LineData d = new LineData();
+        ArrayList<Entry> entries = new ArrayList<>();
+
+        for (int index = 0; index < 12; index++)
+            entries.add(new Entry(getRandom(25, 10), index));
+
+        LineDataSet set = new LineDataSet(entries, "DataSet1");
+        set.setColor(Color.rgb(240, 238, 70));
+        set.setLineWidth(2.5f);
+        set.setCircleColor(Color.rgb(240, 238, 70));
+        set.setCircleSize(5f);
+        set.setFillColor(Color.rgb(240, 238, 70));
+        set.setDrawCubic(true);
+        set.setDrawValues(true);
+        set.setValueTextSize(10f);
+        set.setValueTextColor(Color.rgb(240, 238, 70));
+
+
+        for (int index = 0; index < 12; index++)
+            entries.add(new Entry(getRandom(25, 10), index));
+
+        LineDataSet set2 = new LineDataSet(entries, "DataSet2");
+        set.setColor(Color.rgb(240, 238, 70));
+        set.setLineWidth(2.5f);
+        set.setCircleColor(Color.rgb(240, 238, 70));
+        set.setCircleSize(5f);
+        set.setFillColor(Color.rgb(240, 238, 70));
+        set.setDrawCubic(true);
+        set.setDrawValues(true);
+        set.setValueTextSize(10f);
+        set.setValueTextColor(Color.rgb(240, 238, 70));
+
+        set.setAxisDependency(YAxis.AxisDependency.LEFT);
+
+        d.addDataSet(set);
+        d.addDataSet(set2);
+
+        mChart.setData(d);
+        mChart.setDescription("Fuel Insight");
+        mChart.animateXY(2000, 2000);
+        mChart.invalidate();
+
+//        String[] projection = { FuelContract.COLUMN_AMOUNT, FuelContract.COLUMN_ID };
+//        Cursor fuelData = inflater.getContext().getContentResolver().query(FuelProvider.FUEL_CONTENT_URI, projection, null, null, null);
+//        while (fuelData.getCount()!=0) {
+//            fuelData.moveToNext();
+//            Log.i("FuelData", "ID: " + fuelData.getString(1) + "  Amount: " + fuelData.getString(0));
+//        }
+        return view;
+    }
+
+    private float getRandom(float range, float startsfrom) {
+        return (float) (Math.random() * range) + startsfrom;
     }
 }
